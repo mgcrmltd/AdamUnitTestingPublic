1eb2a60e6643f44c213f4a163c3967ab298b6fff
Mohammad Ghayouba <mohammad@mgcrm.com>
2024-09-29 19:54:34 +0100

    Added methods for names. Email validation not yet written

---
+++ UnitTestIntro/ValidateNewUserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestIntro
+{
+    public interface IValidateNewUserService
+    {
+        bool Email(string email);
+        bool FirstName(string firstname);
+        bool LastName(string lastname);
+    }
+
+    public class ValidateNewUserService : IValidateNewUserService
+    {
+        private const int MinimumFirstNameLength = 2;
+        private const int MinimumLastNameLength = 2;
+
+        public bool Email(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FirstName(string firstname)
+        {
+            if (string.IsNullOrEmpty(firstname)) return false;
+            if(firstname.Length < MinimumFirstNameLength) return false;
+            return true;
+        }
+
+        public bool LastName(string lastname)
+        {
+            if(string.IsNullOrEmpty(lastname)) return false;
+            if(lastname.Length < MinimumLastNameLength) return false;
+            return true;
+        }
+    }
+}
