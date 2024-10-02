e964ac0afcae8cb710cb72d02d9ce732babe4492
Mohammad Ghayouba <mohammad@mgcrm.com>
2024-10-02 16:37:24 +0100

    New email service

---
+++ UnitTestIntro/EmailValidationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestIntro
+{
+    public interface IEmailValidationService
+    {
+        bool Valid(string emailAddress);
+    }
+
+    public class EmailValidationService : IEmailValidationService
+    {
+        public bool Valid(string emailAddress)
+        {
+            return true;
+            //Call external web service
+        }
+    }
+}
---
+++ UnitTestIntro/ValidateNewUserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestIntro
+{
+    public interface IValidateNewUserService
+    {
+        bool Email(string email);
+        bool FirstName(string firstname);
+        bool LastName(string lastname);
+    }
+
+    public class ValidateNewUserService : IValidateNewUserService
+    {
+        private const int MinimumFirstNameLength = 2;
+        private const int MinimumLastNameLength = 2;
+        private IEmailValidationService _emailaValidationService;
+
+        public ValidateNewUserService()
+        {
+            _emailaValidationService = new EmailValidationService();
+        }
+
+        public bool Email(string email)
+        {
+            return _emailaValidationService.Valid(email);
+        }
+
+        public bool FirstName(string firstname)
+        {
+            if (string.IsNullOrEmpty(firstname)) return false;
+            if(firstname.Length < MinimumFirstNameLength) return false;
+            return true;
+        }
+
+        public bool LastName(string lastname)
+        {
+            if(string.IsNullOrEmpty(lastname)) return false;
+            if(lastname.Length < MinimumLastNameLength) return false;
+            return true;
+        }
+    }
+}
