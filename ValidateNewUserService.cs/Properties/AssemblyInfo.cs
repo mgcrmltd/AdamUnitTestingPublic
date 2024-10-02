7890789765c8174b660d5f8a0103912c51fd9464
adamghayouba <adamghayouba@outlook.com>
2024-10-02 10:23:09 -0500

    Unit Test Class for Customer Validation

---
+++ ValidateNewUserService.cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValidateNewUserService.cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValidateNewUserService.cs")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad37a1b1-5622-4817-97ef-da0a7debdea3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ValidateNewUserService.cs/ValidateNewUserServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;  // MSTest
+using NUnit.Framework;  // NUnit
+using UnitTestIntro;    // Reference to the main project
+
+namespace UnitTestIntro.Tests
+{
+   
+    [TestFixture]
+    public class ValidateNewUserServiceNUnitTests
+    {
+        private ValidateNewUserService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new ValidateNewUserService();
+        }
+
+        
+        [Test]
+        public void FirstName_Valid_ReturnsTrue()
+        {
+            var result = _service.FirstName("John");
+            Assert.That(result, Is.True);  
+        }
+
+        [Test]
+        public void FirstName_TooShort_ReturnsFalse()
+        {
+            var result = _service.FirstName("J");
+            Assert.That(result, Is.False);  
+        }
+
+        [Test]
+        public void FirstName_Empty_ReturnsFalse()
+        {
+            var result = _service.FirstName(string.Empty);
+            Assert.That(result, Is.False);  
+        }
+
+       
+        [Test]
+        public void LastName_Valid_ReturnsTrue()
+        {
+            var result = _service.LastName("Doe");
+            Assert.That(result, Is.True);  
+        }
+
+        [Test]
+        public void LastName_TooShort_ReturnsFalse()
+        {
+            var result = _service.LastName("D");
+            Assert.That(result, Is.False);  
+        }
+
+        [Test]
+        public void LastName_Empty_ReturnsFalse()
+        {
+            var result = _service.LastName(string.Empty);
+            Assert.That(result, Is.False);  
+        }
+
+        
+        [Test]
+        public void Email_ThrowsNotImplementedException()
+        {
+            Assert.Throws<NotImplementedException>(() => _service.Email("test@example.com"));
+        }
+    }
+
+    
+} 
