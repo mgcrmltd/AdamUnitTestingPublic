c95d39afa783fe5feb581c7805dd9d1c9d799c1a
Mohammad Ghayouba <mohammad@mgcrm.com>
2024-10-02 16:43:23 +0100

    Tidy up. Remove redundant code

--- UnitTestIntro/EmailValidationService.cs
+++ UnitTestIntro/EmailValidationService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace UnitTestIntro
 {
     public interface IEmailValidationService
@@ -16,7 +11,6 @@ namespace UnitTestIntro
         public bool Valid(string emailAddress)
         {
             return true;
-            //Call external web service
         }
     }
 }
